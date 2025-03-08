5a82b2968caff10ce2aa21cd520dc5a0c7319abf
Nj2514B <144226986+Nj2514B@users.noreply.github.com>
2025-03-08 22:58:46 +0530

    Add files via upload

---
+++ public/insightstream/code/src/context/GeneralContext.jsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export const GeneralContext = React.createContext();
+
+const GeneralContextProvider = ({ children }) => {
+  const [topNews, setTopNews] = useState([]);
+  const [businessNews, setBusinessNews] = useState([]);
+  const [technologyNews, setTechnologyNews] = useState([]);
+  const [politicsNews, setPoliticsNews] = useState([]);
+
+  useEffect(() => {
+    fetchTopNews();
+    fetchBusinessNews();
+    fetchPoliticsNews();
+    fetchTechnologyNews();
+  }, []);
+
+  const fetchTopNews = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/everything?q=popular&apiKey=324ed8976a6a4ce99f2d3e2244c4cbc6"
+      );
+      setTopNews(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchBusinessNews = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/everything?q=business&apiKey=324ed8976a6a4ce99f2d3e2244c4cbc6"
+      );
+      setBusinessNews(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const fetchPoliticsNews = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/everything?q=politics&apiKey=324ed8976a6a4ce99f2d3e2244c4cbc6"
+      );
+      setPoliticsNews(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const fetchTechnologyNews = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/everything?q=technology&apiKey=324ed8976a6a4ce99f2d3e2244c4cbc6"
+      );
+      setTechnologyNews(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <GeneralContext.Provider
+      value={{ topNews, businessNews, technologyNews, politicsNews }}
+    >
+      {children}
+    </GeneralContext.Provider>
+  );
+};
+
+export default GeneralContextProvider;
